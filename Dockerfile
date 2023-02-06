d329d0e43bd6ec42d7a967c70d72d065602ffd48
nishant <nyindorkar@gmail.com>
2023-02-06 12:09:57 +0530

    php-mod

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM php:7.2.6-apache
+FROM php:7.1.2-apache 
 RUN docker-php-ext-install mysqli
--- www/index.php
+++ www/index.php
@@ -1,34 +1,46 @@
-<?php
-$conn = mysqli_connect('db', 'user', 'test', 'myDb', 3306);
-mysqli_set_charset($conn, "utf8");
-$query = 'SELECT * From Person';
-$result = mysqli_query($conn, $query);
-?>
+<!-- put in ./www directory -->
 
-<!DOCTYPE html>
-<html lang="en">
+<html>
+ <head>
+  <title>Hello...</title>
 
-<head>
-  <meta charset="utf-8">
-  <title>Demo</title>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-  <style>body{font-family:Nunito,sans-serif;min-height:90vh;align-items:center;justify-content:center;display:flex}</style>
+  <meta charset="utf-8"> 
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 </head>
-
 <body>
-  <p>
-    <?php
-    while ($value = $result->fetch_array(MYSQLI_ASSOC)) {
-      foreach ($value as $element) {
-        echo ' - ' . $element;
-      }
-    }
-    $result->close();
-    mysqli_close($conn);
-    ?>
-    -
-  </p>
-</body>
+    <div class="container">
+    <?php echo "<h1>Hi! I'm happy</h1>"; ?>
 
+    <?php
+
+    $conn = mysqli_connect('db', 'user', 'test', "myDb");
+
+
+    $query = 'SELECT * From Person';
+    $result = mysqli_query($conn, $query);
+
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+    while($value = $result->fetch_array(MYSQLI_ASSOC)){
+        echo '<tr>';
+        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+        foreach($value as $element){
+            echo '<td>' . $element . '</td>';
+        }
+
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    $result->close();
+
+    mysqli_close($conn);
+
+    ?>
+    </div>
+</body>
 </html>
